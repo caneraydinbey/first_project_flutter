7db3625c9993d63365bc1b7cc3c31d196dee3038
Caner Aydın <02485494@ksnet.local>
2024-02-29 16:35:00 +0300

    Initial commit

---
+++ lib/styled_text.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class StyledText extends StatelessWidget {
+  const StyledText();
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Hqqellfffffffo',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 38,
+      ),
+    );
+  }
+
+
+}
